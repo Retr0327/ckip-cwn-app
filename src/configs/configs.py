8ce6878526a2ea03dec72d3c8f64b95d1c9e6976
Retr0327 <philcoke35@gmail.com>
2022-08-22 02:14:34 +0800

    Update download_ckip_drivers

--- src/configs/configs.py
+++ src/configs/configs.py
@@ -5,7 +5,6 @@ from pathlib import Path
 pkg_path = Path("__file__").resolve().parent / "src"
 ckip_dir = pkg_path / "models"
 ckip_path = ckip_dir / "ckip_drivers.pickle"
-has_ckip_path = Path(ckip_path).exists()
 
 
 def download_ckip_drivers():
@@ -15,7 +14,7 @@ def download_ckip_drivers():
 
     NLP_MODEL = "bert-base"
 
-    if not has_ckip_path:
+    if not Path(ckip_path).exists():
         from ckip_transformers.nlp import CkipWordSegmenter, CkipPosTagger
 
         drivers = (CkipWordSegmenter(model=NLP_MODEL), CkipPosTagger(model=NLP_MODEL))

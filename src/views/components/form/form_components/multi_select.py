a5122763eb99a13d476d4f3914fe3f5edbf9ec16
Retr0327 <philcoke35@gmail.com>
2022-08-22 15:42:43 +0800

    Add multiselect

---
+++ src/views/components/form/form_components/multi_select.py
@@ -0,0 +1,13 @@
+from streamlit import sidebar
+from typing import Callable, List, Optional
+
+
+def add_multiselect(
+    title: str,
+    options: List[str],
+    key: Optional[str] = None,
+    on_change: Optional[Callable] = None,
+):
+    return sidebar.multiselect(
+        title, options=options, default=list(options), key=key, on_change=on_change
+    )

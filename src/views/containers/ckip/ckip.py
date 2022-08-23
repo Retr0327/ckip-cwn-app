a3d8a58831589a14fabde234d8336a0fb6197e41
Retr0327 <philcoke35@gmail.com>
2022-08-23 18:39:26 +0800

    Add display_ckip

--- src/views/containers/ckip/ckip.py
+++ src/views/containers/ckip/ckip.py
@@ -1,11 +1,11 @@
 from typing import List
 from ...services import request
-from streamlit import expander, markdown
+from streamlit import expander, markdown, write
 
 divider_tag = "<hr style='margin-top: 1rem;margin-bottom: 1rem;border: 0;border-top: 1px solid rgba(0,0,0,.1);'/>"
 
 
-def create_data_card(visualizer: str, sentence_list: List[str]):
+def create_expander(visualizer: str, sentence_list: List[str]):
     fetch_method = list(visualizer.keys())[0]
     title = visualizer[fetch_method]
     html_tags = request(fetch_method, sentence_list)
@@ -13,3 +13,8 @@ def create_data_card(visualizer: str, sentence_list: List[str]):
 
     with expander(title, expanded=True):
         markdown(html_string, unsafe_allow_html=True)
+
+
+def display_ckip(visualizers: List[str], sentence_list: List[str]):
+    for visualizer in visualizers:
+        create_expander(visualizer, sentence_list)
--- src/views/containers/data_card/__init__.py
+++
@@ -1,4 +0,0 @@
-from .card import create_data_card
-
-
-__all__ = ["create_data_card"]

debf5e8050ca9b86bbe03a9560d46cffd4f17217
Retr0327 <philcoke35@gmail.com>
2022-08-23 13:10:07 +0800

    Add format_option

--- src/views/components/sidebar/sidebar.py
+++ src/views/components/sidebar/sidebar.py
@@ -1,18 +1,22 @@
-import streamlit as st
+from typing import Dict
 from ..form import form_controller
+from streamlit import sidebar, image, session_state
 from .options import MODEL_OPTIONS, CKIP_VISUALIZERS, CWN_VISUALIZERS
 
 
 def remove_input_data():
-    if "input_data" in st.session_state:
-        del st.session_state["input_data"]
+    if "input_data" in session_state:
+        del session_state["input_data"]
+
+
+def format_option(option: Dict[str, str]) -> str:
+    """The format_options function gets the value of a dict."""
+    return list(option.values())[0]
 
 
 def visualize_side_bar():
-    with st.sidebar:
-        st.image(
-            "https://avatars.githubusercontent.com/u/21136511?s=200&v=4", width=100
-        )
+    with sidebar:
+        image("https://avatars.githubusercontent.com/u/21136511?s=200&v=4", width=100)
 
         model_options = form_controller(
             control="select-box",
@@ -24,7 +28,10 @@ def visualize_side_bar():
         visualizers = CKIP_VISUALIZERS if model_options == "CKIP" else CWN_VISUALIZERS
 
         active_visualizers = form_controller(
-            control="multi-select", title="Visualizers", options=visualizers
+            control="multi-select",
+            title="Visualizers",
+            options=visualizers,
+            format_func=format_option,
         )
-        
+
         return active_visualizers

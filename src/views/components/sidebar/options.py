c252803363d4b98573dff9a4d81b2fd97550af79
Retr0327 <philcoke35@gmail.com>
2022-08-23 13:07:46 +0800

    Update options format

--- src/views/components/sidebar/options.py
+++ src/views/components/sidebar/options.py
@@ -1,3 +1,3 @@
 MODEL_OPTIONS = ("CKIP", "CWN")
-CKIP_VISUALIZERS = ["斷詞系統", "實體辨識"]
-CWN_VISUALIZERS = ["VIS"]
+CKIP_VISUALIZERS = [{"pos": "斷詞系統"}, {"ner": "實體辨識"}]
+CWN_VISUALIZERS = [{"VIS": "VIS"}]

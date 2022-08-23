046290426a2b676a056cf4600667048b47b79d82
Retr0327 <philcoke35@gmail.com>
2022-08-23 19:19:13 +0800

    Add handle_cwn_sense_tag

--- src/views/services/request.py
+++ src/views/services/request.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from typing import Union
+from controllers.cwn import handle_cwn_sense_tag
 from controllers.ckip import handle_pos_tagging, handle_ner_chunker
 
 TEN_MINUTES = 60 * 10
@@ -8,6 +9,11 @@ TEN_MINUTES = 60 * 10
 @st.cache(ttl=TEN_MINUTES, show_spinner=False)
 def request(method: str, *args, **kwargs) -> Union[str, tuple]:
     """The fetch function fetch the data in the database based on the `method`."""
-    methods = {"pos": handle_pos_tagging, "ner": handle_ner_chunker}
+
+    methods = {
+        "pos": handle_pos_tagging,
+        "ner": handle_ner_chunker,
+        "distil_tag": handle_cwn_sense_tag,
+    }
 
     return methods[method](*args, **kwargs)

be5da7a99c12a4e0ba8a11bab4a1a7168f24c8c3
Retr0327 <philcoke35@gmail.com>
2022-08-23 15:09:37 +0800

    Update run_app

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
 import streamlit as st
+from pathlib import Path
+from configs import download_ckip_drivers, ckip_path
+from views import create_data_form, visualize_side_bar, create_data_card
 
 
 def run_app() -> None:
-    st.title("App")
+    while not Path(ckip_path).exists():
+        with st.spinner("Downloading ckip models ..."):
+            download_ckip_drivers()
+
+    st.title("LOPE")
+    input_data = create_data_form()
+    active_visualizers = visualize_side_bar()
+
+    if "input_data" in st.session_state and input_data:
+        for visualizer in active_visualizers:
+            create_data_card(visualizer, st.session_state["input_data"])
 
 
 if __name__ == "__main__":

00d955150d6990c66499f4e4d44c75a9b79cdbc2
Retr0327 <philcoke35@gmail.com>
2022-08-24 13:02:07 +0800

    Add display_cwn

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,5 +1,5 @@
 from .components import visualize_side_bar
-from .containers import create_data_form, create_data_card
+from .containers import create_data_form, display_ckip, display_cwn
 
 
-__all__ = ["create_data_form", "visualize_side_bar", "create_data_card"]
+__all__ = ["create_data_form", "visualize_side_bar", "display_ckip", "display_cwn"]

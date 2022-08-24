dc1a9d991455328eded9c74451cdfe8e047c28b3
Retr0327 <philcoke35@gmail.com>
2022-08-25 01:06:42 +0800

    Remove pkg_loader

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,11 +1,10 @@
-from .components import visualize_side_bar, load_package
+from .components import visualize_side_bar
 from .containers import create_data_form, display_ckip, display_cwn
 
 
 __all__ = [
     "create_data_form",
     "visualize_side_bar",
-    "load_package",
     "display_ckip",
     "display_cwn",
 ]
--- src/views/components/__init__.py
+++ src/views/components/__init__.py
@@ -1,6 +1,5 @@
 from .form import form_controller
-from .pkg_loader import load_package
 from .sidebar import visualize_side_bar
 
 
-__all__ = ["form_controller", "load_package", "visualize_side_bar"]
+__all__ = ["form_controller", "visualize_side_bar"]

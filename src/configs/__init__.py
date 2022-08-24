76b145ab04a39be8410a7833ba05f0ee623f8be4
Retr0327 <philcoke35@gmail.com>
2022-08-25 01:12:26 +0800

    Add download_cwn_models

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -1,4 +1,14 @@
-from .configs import ckip_path, download_ckip_drivers
+from .configs import (
+    ckip_path,
+    download_ckip_drivers,
+    download_cwn_models,
+    cwn_model_path,
+)
 
 
-__all__ = ["ckip_path", "download_ckip_drivers" ]
+__all__ = [
+    "ckip_path",
+    "download_ckip_drivers",
+    "download_cwn_models",
+    "cwn_model_path",
+]

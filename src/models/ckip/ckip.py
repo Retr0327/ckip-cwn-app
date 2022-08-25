c3880717cb7cc55fc3f24a334b2504c2434ccf10
Retr0327 <philcoke35@gmail.com>
2022-08-26 00:51:34 +0800

    Update connect_ckip_drivers

--- src/models/ckip/ckip.py
+++ src/models/ckip/ckip.py
@@ -2,12 +2,17 @@ import pickle
 from configs import ckip_path
 
 
-def connect_ckip_drivers() -> tuple:
+def connect_ckip_drivers(nlp_model: str) -> tuple:
     """The connect_ckip_drivers function connects to the ckip drivers.
 
+    Args:
+        nlp_model (str): the nlp model name
     Returns:
         a tuple, containing CkipWordSegmenter, CkipPosTagger and CkipNerChunker.
     """
 
-    with open(ckip_path, "rb") as file:
+    driver_path = ckip_path / f"{nlp_model}_drivers.pickle"
+    print('driver_path', driver_path)
+
+    with open(driver_path, "rb") as file:
         return pickle.load(file)

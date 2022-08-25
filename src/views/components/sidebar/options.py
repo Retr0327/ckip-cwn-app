a5df7f303aeff64bbb66b0c77274dcd8365b6f1f
Retr0327 <philcoke35@gmail.com>
2022-08-25 15:39:24 +0800

    Add `ws` option

--- src/views/components/sidebar/options.py
+++ src/views/components/sidebar/options.py
@@ -1,3 +1,3 @@
 MODEL_OPTIONS = ("CKIP", "CWN")
-CKIP_VISUALIZERS = [{"pos": "斷詞系統"}, {"ner": "實體辨識"}]
+CKIP_VISUALIZERS = [{"ws": "斷詞系統"}, {"pos": "詞類標記"}, {"ner": "實體辨識"}]
 CWN_VISUALIZERS = ["DistilTag", "CwnSenseTag"]

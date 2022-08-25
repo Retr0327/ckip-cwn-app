c6725ad3acffdc0634f57eb2933ecb72edc7bb53
Retr0327 <philcoke35@gmail.com>
2022-08-26 00:40:47 +0800

    Add model_options

--- src/views/components/sidebar/sidebar.py
+++ src/views/components/sidebar/sidebar.py
@@ -1,12 +1,13 @@
+import streamlit as st
 from typing import Dict, Union
 from ..form import form_controller
-from streamlit import sidebar, image, session_state
-from .options import MODEL_OPTIONS, CKIP_VISUALIZERS, CWN_VISUALIZERS
+from configs import download_ckip_drivers
+from .options import MODEL_OPTIONS, PIPELINE_OPTIONS, CKIP_VISUALIZERS, CWN_VISUALIZERS
 
 
 def remove_input_data():
-    if "input_data" in session_state:
-        del session_state["input_data"]
+    if "input_data" in st.session_state:
+        del st.session_state["input_data"]
 
 
 def format_option(option: Union[str, Dict[str, str]]) -> str:
@@ -26,17 +27,30 @@ def format_option(option: Union[str, Dict[str, str]]) -> str:
 
 
 def visualize_side_bar():
-    with sidebar:
-        image("https://avatars.githubusercontent.com/u/21136511?s=200&v=4", width=100)
+    with st.sidebar:
+        st.image(
+            "https://avatars.githubusercontent.com/u/21136511?s=200&v=4", width=100
+        )
 
-        model_options = form_controller(
+        pipeline_options = form_controller(
             control="select-box",
             title="中文 NLP 管線處理：",
-            options=MODEL_OPTIONS,
+            options=PIPELINE_OPTIONS,
             on_change=remove_input_data,
         )
 
-        visualizers = CKIP_VISUALIZERS if model_options == "CKIP" else CWN_VISUALIZERS
+        model_options = None
+        if pipeline_options == "CKIP":
+            model_options = form_controller(
+                control="select-box",
+                title="NLP 模型：",
+                options=MODEL_OPTIONS,
+                key="model",
+            )
+
+        visualizers = (
+            CKIP_VISUALIZERS if pipeline_options == "CKIP" else CWN_VISUALIZERS
+        )
 
         active_visualizers = form_controller(
             control="multi-select",
@@ -45,4 +59,4 @@ def visualize_side_bar():
             format_func=format_option,
         )
 
-        return model_options, active_visualizers
+        return model_options, pipeline_options, active_visualizers

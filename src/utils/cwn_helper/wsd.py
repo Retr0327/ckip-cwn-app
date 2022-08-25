c196c2c9ce39f5ad969ba8a4ad4cecacc8826ee0
Retr0327 <philcoke35@gmail.com>
2022-08-25 15:13:45 +0800

    Update create_cwn_sense_tags

--- src/utils/cwn_helper/wsd.py
+++ src/utils/cwn_helper/wsd.py
@@ -1,20 +1,38 @@
+import asyncio
 from typing import List, Tuple
 from DistilTag import DistilTag
 from CwnSenseTagger import senseTag
 
+
+# --------------------------------------------------------------------
+# distil tag
+
 tagger = DistilTag()
 
-def disambiguate_word_sense(sentence_list: List[str])-> List[Tuple[str]]:
+
+async def tag_values(value):
+    return tagger.tag(value)
+
+
+async def disambiguate_word_sense(sentence_list: List[str]) -> List[Tuple[str]]:
     """The disambiguate_word_sense function disambiuates the word sense.
-    
+
     Args:
         sentence_list (list): a list of sentences
     Returns:
         a list of tuples.
     """
-    disambiguate = lambda value: tagger.tag(value)
-    return list(map(disambiguate, sentence_list))
 
-def create_cwn_sense_tags(disambiguated_list: List[List[List[tuple]]]):
-    create = lambda value: senseTag(value)
-    return list(map(create, disambiguated_list))
+    return await asyncio.gather(*list(map(tag_values, sentence_list)))
+
+
+# --------------------------------------------------------------------
+# CWN sense tag
+
+
+async def sense_value(value):
+    return senseTag(value)
+
+
+async def create_cwn_sense_tags(disambiguated_list: List[List[List[tuple]]]):
+    return await asyncio.gather(*list(map(sense_value, disambiguated_list)))

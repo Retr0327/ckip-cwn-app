41eed6da321f0ab8614b0ee8fc17a149dd02890f
Retr0327 <philcoke35@gmail.com>
2022-08-26 01:21:59 +0800

    Update display_ckip

--- src/views/containers/ckip/ckip.py
+++ src/views/containers/ckip/ckip.py
@@ -5,16 +5,16 @@ from ...services import request
 divider_tag = "<hr style='margin-top: 1rem;margin-bottom: 1rem;border: 0;border-top: 1px solid rgba(0,0,0,.1);'/>"
 
 
-def create_expander(visualizer: str, sentence_list: List[str]):
+def create_expander(nlp_model: str, visualizer: str, sentence_list: List[str]):
     fetch_method = list(visualizer.keys())[0]
     title = visualizer[fetch_method]
-    html_tags = request(fetch_method, sentence_list)
+    html_tags = request(fetch_method, nlp_model, sentence_list)
     html_string = divider_tag.join(html_tags)
 
     with st.expander(title, expanded=True):
         st.markdown(html_string, unsafe_allow_html=True)
 
 
-def display_ckip(visualizers: List[str], sentence_list: List[str]):
+def display_ckip(nlp_model: str, visualizers: List[str], sentence_list: List[str]):
     for visualizer in visualizers:
-        create_expander(visualizer, sentence_list)
+        create_expander(nlp_model, visualizer, sentence_list)

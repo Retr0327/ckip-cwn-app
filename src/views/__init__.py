0a96aa738f62b3ef387597e88208f13aa0287548
Retr0327 <philcoke35@gmail.com>
2022-08-26 01:23:29 +0800

    Add dowload_ckip_package

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,5 +1,11 @@
-from .components import visualize_side_bar
+from .components import visualize_side_bar, dowload_ckip_package
 from .containers import create_data_form, display_ckip, display_cwn
 
 
-__all__ = ["create_data_form", "visualize_side_bar", "display_ckip", "display_cwn"]
+__all__ = [
+    "create_data_form",
+    "visualize_side_bar",
+    "display_ckip",
+    "display_cwn",
+    "dowload_ckip_package",
+]

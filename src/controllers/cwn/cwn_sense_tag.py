1b8a8fc86f050f789afdbb7969ec7476433234eb
Retr0327 <philcoke35@gmail.com>
2022-08-25 15:26:23 +0800

    Add create_tags

--- src/controllers/cwn/cwn_sense_tag.py
+++ src/controllers/cwn/cwn_sense_tag.py
@@ -1,24 +1,30 @@
 import asyncio
 from typing import List
-from itertools import chain
-from utils import disambiguate_word_sense, create_cwn_sense_tags, create_pos_color
+from utils import (
+    disambiguate_word_sense,
+    create_cwn_sense_tags,
+    add_multiple_textsubscripts,
+)
 
 
-async def add_textsubscript(segmented_list) -> str:
+async def create_tags(segmented_result):
+    """The create_tags function runs two asynchronous operations (i.e.
+    `add_multiple_textsubscripts` and `create_cwn_sense_tags`).
 
-    create = (
-        lambda value: f"<span>{value[0]}<sub style='margin-right: 1rem; color: {create_pos_color(value[1])};'>{value[1]}</sub></span>"
+    Args:
+        segmented_result (list)
+    """
+
+    return await asyncio.gather(
+        *[
+            add_multiple_textsubscripts("cwn", segmented_result),
+            create_cwn_sense_tags(segmented_result),
+        ]
     )
-    return "".join(list(map(create, chain(*segmented_list))))
-
-
-async def add_multiple_textsubscripts(segmented_result):
-    return await asyncio.gather(*list(map(add_textsubscript, segmented_result)))
 
 
 def handle_cwn_sense_tag(sentence_list: List[str]) -> List[str]:
-    segmented_result = disambiguate_word_sense(sentence_list)
-    span_tags = asyncio.run(add_multiple_textsubscripts(segmented_result))
-    cwn_tags = create_cwn_sense_tags(segmented_result)
+    segmented_result = asyncio.run(disambiguate_word_sense(sentence_list))
+    span_tags, cwn_tags = asyncio.run(create_tags(segmented_result))
 
     return span_tags, cwn_tags

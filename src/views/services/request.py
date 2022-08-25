5074795896091992f6d3bbd614e375820e18ddf8
Retr0327 <philcoke35@gmail.com>
2022-08-25 15:38:42 +0800

    Add handle_word_segmentation

--- src/views/services/request.py
+++ src/views/services/request.py
@@ -1,19 +1,24 @@
 import streamlit as st
 from typing import Union
+from controllers.ckip import (
+    handle_pos_tagging,
+    handle_ner_chunker,
+    handle_word_segmentation,
+)
 from controllers.cwn import handle_cwn_sense_tag
-from controllers.ckip import handle_pos_tagging, handle_ner_chunker
 
 TEN_MINUTES = 60 * 10
 
 
-@st.cache(ttl=TEN_MINUTES, show_spinner=False)
-def request(method: str, *args, **kwargs) -> Union[str, tuple]:
+@st.cache(ttl=TEN_MINUTES, show_spinner=True)
+def request(method: str, *args, **kwargs):
     """The fetch function fetch the data in the database based on the `method`."""
 
     methods = {
+        "ws": handle_word_segmentation,
         "pos": handle_pos_tagging,
         "ner": handle_ner_chunker,
-        "distil_tag": handle_cwn_sense_tag,
+        "cwn": handle_cwn_sense_tag,
     }
 
     return methods[method](*args, **kwargs)

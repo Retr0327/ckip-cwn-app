62fddb2f06f19ecc281d3f1955af09e940312a5b
Retr0327 <philcoke35@gmail.com>
2022-08-26 01:20:23 +0800

    Add MODEL_OPTIONS

--- src/views/components/sidebar/options.py
+++ src/views/components/sidebar/options.py
@@ -1,3 +1,4 @@
-MODEL_OPTIONS = ("CKIP", "CWN")
+MODEL_OPTIONS = ["bert-base", "albert-tiny", "albert-base", "bert-tiny"]
+PIPELINE_OPTIONS = ("CKIP", "CWN")
 CKIP_VISUALIZERS = [{"ws": "斷詞系統"}, {"pos": "詞類標記"}, {"ner": "實體辨識"}]
 CWN_VISUALIZERS = ["DistilTag", "CwnSenseTag"]

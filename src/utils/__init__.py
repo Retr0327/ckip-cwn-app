9ebe61c9cc2381a5bbc8cfd30c6d05bcaab02d4d
Retr0327 <philcoke35@gmail.com>
2022-08-25 13:32:30 +0800

    Add add_multiple_textsubscripts

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,13 @@
-from .ckip_helper import Segmenter, chunk_multiple_entities
+from .textsubscript import add_multiple_textsubscripts
 from .cwn_helper import disambiguate_word_sense, create_cwn_sense_tags
+from .ckip_helper import PosTagging, WordSegmentation, chunk_multiple_entities
 
 
 __all__ = [
     "disambiguate_word_sense",
-    "Segmenter",
+    "PosTagging",
+    "WordSegmentation",
     "chunk_multiple_entities",
     "create_cwn_sense_tags",
+    "add_multiple_textsubscripts",
 ]

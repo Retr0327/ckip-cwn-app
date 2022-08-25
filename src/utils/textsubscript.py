4ff907efbdd13d8ad86afa34843cfcb67c557f22
Retr0327 <philcoke35@gmail.com>
2022-08-25 15:19:34 +0800

    Add add_cwn_textsubscript

--- src/utils/textsubscript.py
+++ src/utils/textsubscript.py
@@ -1,5 +1,6 @@
 import asyncio
-from typing import List
+from itertools import chain
+from typing import List, Union
 from .text_color import create_pos_color
 
 
@@ -16,13 +17,21 @@ async def add_ws_textsubscript(data_list: List[str]) -> str:
     return "".join(list(map(create, data_list)))
 
 
+async def add_cwn_textsubscript(segmented_list) -> str:
+    create = (
+        lambda value: f"<span>{value[0]}<sub style='margin-right: 1rem; color: {create_pos_color(value[1])};'>{value[1]}</sub></span>"
+    )
+    return "".join(list(map(create, chain(*segmented_list))))
+
+
 async def add_multiple_textsubscripts(
-    target: str, list_of_lists: List[List[str]]
+    target: str, list_of_lists: List[List[Union[str, tuple]]]
 ) -> List[str]:
 
     textsubscript_factories = {
         "ws": add_ws_textsubscript,
         "pos": add_pos_textsubscript,
+        "cwn": add_cwn_textsubscript,
     }
 
     return await asyncio.gather(

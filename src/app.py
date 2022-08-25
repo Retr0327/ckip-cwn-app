9560aaf32cb9de3db506642adf0c87eeec17f9ff
Retr0327 <philcoke35@gmail.com>
2022-08-26 01:22:24 +0800

    Update run_app

--- src/app.py
+++ src/app.py
@@ -1,21 +1,25 @@
+from views import (
+    display_cwn,
+    display_ckip,
+    create_data_form,
+    visualize_side_bar,
+    dowload_ckip_package,
+)
 import streamlit as st
-from pathlib import Path
-from configs import download_ckip_drivers, ckip_path
-from views import create_data_form, visualize_side_bar, create_data_card
 
 
 def run_app() -> None:
-    while not Path(ckip_path).exists():
-        with st.spinner("Downloading ckip models ..."):
-            download_ckip_drivers()
+    dowload_ckip_package()
 
     st.title("LOPE")
     input_data = create_data_form()
-    active_visualizers = visualize_side_bar()
+    model, pipeline, active_visualizers = visualize_side_bar()
+    display_factories = {"CKIP": display_ckip, "CWN": display_cwn}
 
-    if "input_data" in st.session_state and input_data:
-        for visualizer in active_visualizers:
-            create_data_card(visualizer, st.session_state["input_data"])
+    if "input_data" in st.session_state:
+        display_factories[pipeline](
+            model, active_visualizers, st.session_state["input_data"]
+        )
 
 
 if __name__ == "__main__":

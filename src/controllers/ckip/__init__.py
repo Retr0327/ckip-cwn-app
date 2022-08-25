f902fcb2fe8477d4835c7044da70ba32c861afaa
Retr0327 <philcoke35@gmail.com>
2022-08-25 13:02:14 +0800

    Add handle_word_segmentation

--- src/controllers/ckip/__init__.py
+++ src/controllers/ckip/__init__.py
@@ -1,5 +1,6 @@
 from .ner_chunker import handle_ner_chunker
 from .pos_tagging import handle_pos_tagging
+from .word_segmentation import handle_word_segmentation
 
 
-__all__ = ["handle_ner_chunker", "handle_pos_tagging"]
+__all__ = ["handle_ner_chunker", "handle_pos_tagging", "handle_word_segmentation"]

b0c108c6230af52c2611a302f24de610a0648107
Retr0327 <philcoke35@gmail.com>
2022-08-26 13:14:48 +0800

    Remove MODEL_OPTIONS

--- src/views/components/sidebar/options.py
+++ src/views/components/sidebar/options.py
@@ -1,4 +1,3 @@
-MODEL_OPTIONS = ["bert-base", "albert-tiny", "albert-base", "bert-tiny"]
 PIPELINE_OPTIONS = ("CKIP", "CWN")
-CKIP_VISUALIZERS = [{"ws": "斷詞系統"}, {"pos": "詞類標記"}, {"ner": "實體辨識"}]
 CWN_VISUALIZERS = ["DistilTag", "CwnSenseTag"]
+CKIP_VISUALIZERS = [{"ws": "斷詞系統"}, {"pos": "詞類標記"}, {"ner": "實體辨識"}]

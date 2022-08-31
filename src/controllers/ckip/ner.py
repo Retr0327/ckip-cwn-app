43db4a56dfb23f707d2e061d06a0f3a62eaccbfa
Retr0327 <philcoke35@gmail.com>
2022-09-01 01:22:59 +0800

    Add handle_create_ner

--- src/controllers/ckip/ner.py
+++ src/controllers/ckip/ner.py
@@ -1,13 +1,12 @@
 import asyncio
 from typing import List, Union
 from models import connect_ckip_drivers
-from utils import chunk_multiple_entities
 from ckip_transformers.nlp.util import NerToken
+from utils.ckip.ner import chunk_multiple_entities
 
 
 def is_list_of_empty_list(ner_token_list: List[Union[NerToken, None]]) -> bool:
     """The is_list_of_empty_list function checks whether a list is full of empty lists.
-
     Args:
         ner_token_list (list): the result of the ner driver
     Returns:
@@ -16,9 +15,8 @@ def is_list_of_empty_list(ner_token_list: List[Union[NerToken, None]]) -> bool:
     return all(map(lambda value: not value, ner_token_list))
 
 
-def handle_ner_chunker(nlp_model: str, sentence_list: List[str]) -> List[str]:
-    """The handle_ner_chunker function handles the request that deals with NER.
-
+def handle_create_ner(nlp_model: str, sentence_list: List[str]) -> List[str]:
+    """The handle_create_ner function handles the request that deals with NER.
     Args:
         nlp_model (str): the nlp model name
         sentence_list (list): a list of sentences
--- src/controllers/ckip/ner_chunker/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ner_chunker import handle_ner_chunker
-
-
-__all__ = ['handle_ner_chunker']

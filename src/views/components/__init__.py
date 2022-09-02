76626f4b3deb283aacc012163c91726009d7d45f
Retr0327 <philcoke35@gmail.com>
2022-09-03 00:28:19 +0800

    Add download_cwn_drivers

--- src/views/components/__init__.py
+++
@@ -1,6 +0,0 @@
-from .form import form_controller
-from .sidebar import visualize_side_bar
-from .ckip_pkg_loader import dowload_ckip_package
-
-
-__all__ = ["form_controller", "visualize_side_bar", 'dowload_ckip_package']
